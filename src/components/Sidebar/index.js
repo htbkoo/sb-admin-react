190235b68c40def0d0c143bfa17c50955a5fd7e2
htbkoo <h1017760@connect.hku.hk>
2018-01-05 13:48:14 +0800

    cleaned Sidebar/index.js

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -3,17 +3,6 @@ import SidebarContent from "./SidebarContent";
 
 class Sidebar extends Component {
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      uiElementsCollapsed: true,
-      chartsElementsCollapsed: true,
-      multiLevelDropdownCollapsed: true,
-      thirdLevelDropdownCollapsed: true,
-      samplePagesCollapsed: true,
-    };
-  }
-
   render() {
     return <SidebarContent/>;
   }
