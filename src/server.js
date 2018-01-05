b38554fc030863ef39406b36d6d1ee0aab8d7c4e
htbkoo <h1017760@connect.hku.hk>
2018-01-05 13:46:21 +0800

    suppress IntelliJ warning

--- src/server.js
+++ src/server.js
@@ -17,6 +17,7 @@ import errorPageStyle from './routes/error/ErrorPage.css';
 // import models from './data/models';
 // import schema from './data/schema';
 import routes from './routes';
+// noinspection JSFileReferences
 import assets from './assets'; // eslint-disable-line import/no-unresolved
 import { port, auth } from './config';
 
